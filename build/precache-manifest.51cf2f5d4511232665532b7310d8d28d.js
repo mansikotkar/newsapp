cabf44eec9223c5b8953e28075266575c6980129
mansikotkar <86104677+mansikotkar@users.noreply.github.com>
2022-05-06 22:34:44 +0530

    Add files via upload

---
+++ build/precache-manifest.51cf2f5d4511232665532b7310d8d28d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "429917ad4703341ccba7",
+    "url": "/newsapp/static/css/main.f7a002ea.chunk.css"
+  },
+  {
+    "revision": "429917ad4703341ccba7",
+    "url": "/newsapp/static/js/main.429917ad.chunk.js"
+  },
+  {
+    "revision": "aea4a5bdfab3b8beece5",
+    "url": "/newsapp/static/js/1.aea4a5bd.chunk.js"
+  },
+  {
+    "revision": "b80a43043315514f7e42",
+    "url": "/newsapp/static/css/2.201ef5be.chunk.css"
+  },
+  {
+    "revision": "b80a43043315514f7e42",
+    "url": "/newsapp/static/js/2.b80a4304.chunk.js"
+  },
+  {
+    "revision": "0e0315c88f2328ef80f2",
+    "url": "/newsapp/static/js/runtime~main.0e0315c8.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/newsapp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/newsapp/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/newsapp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/newsapp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "b2041abc80f24f0ef06e90c879d0112d",
+    "url": "/newsapp/index.html"
+  }
+];
